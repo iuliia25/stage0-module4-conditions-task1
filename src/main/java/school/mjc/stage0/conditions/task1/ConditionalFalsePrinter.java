f42930a698293ae207bdd9f5a7841725036a5def
iuliia25 <khlystun.iuliia@gmail.com>
2022-09-06 16:44:51 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -1,6 +1,14 @@
 package school.mjc.stage0.conditions.task1;
 
+
+
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean a = (8 < 6);
+        if (a) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean a = (6 > 4);
+        if (a) {
+            System.out.println(a);
+        }
     }
 }
