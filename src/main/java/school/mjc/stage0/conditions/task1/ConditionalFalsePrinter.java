8c911ec29904f0e09684630a7b544bd4e3e670f5
iuliia25 <khlystun.iuliia@gmail.com>
2022-09-06 16:47:37 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -6,9 +6,9 @@ public class ConditionalFalsePrinter {
     public static void main(String[] args) {
         boolean a = (8 < 6);
         if (a) {
-            System.out.println("True");
+            System.out.println("true");
         } else {
-            System.out.println("False");
+            System.out.println("false");
         }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.conditions.task1;
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
         if (numberToBeDetermined > 0) {
-            System.out.println("True");
+            System.out.println("true");
         } else {
-            System.out.println("False");
+            System.out.println("false");
         }
     }
 }
